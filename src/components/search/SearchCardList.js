ede056f374ca727790c19a5393bebb9eae1a37e9
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-09-03 00:46:19 +0530

    bug fix

--- src/components/search/SearchCardList.js
+++ src/components/search/SearchCardList.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import SearchCard from './SearchCard';
 import { useSelector } from 'react-redux';
 import { GOOGLE_API_KEY, SEARCH_VIDEOS } from '../../utils/constants';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 const SearchCardList = () => {
   const searchQuery = useSelector((store) => store.search.searchContent);
   console.log('search query from card', searchQuery);
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const videoContent = searchParams.get('search_query');
   console.log('video Content', videoContent);
@@ -20,13 +21,15 @@ const SearchCardList = () => {
   }, [searchQuery]);
   console.log('search query is here inside searchCard list', searchQuery);
   const getSearchVidoes = async () => {
-    if (searchQuery !== '') {
+    if (searchQuery !== '' && searchQuery !== null) {
       const res = await fetch(
         `${SEARCH_VIDEOS}${searchQuery}&type=video${GOOGLE_API_KEY}`
       );
       const searchVideos = await res.json();
       console.log('searched videos', searchVideos);
       setSearchContents(searchVideos.items);
+    } else {
+      navigate('/', { replace: true });
     }
   };
 
