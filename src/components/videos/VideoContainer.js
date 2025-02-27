2c2952839e0a6fbd30538bfc5c95c096900499db
Nizamph <nizamph7@gmail.com>
2025-02-27 10:24:01 +0530

    [FTX] width change

--- src/components/videos/VideoContainer.js
+++ src/components/videos/VideoContainer.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import VideoList from './VideoList';
-import { useDispatch } from 'react-redux';
-import { closeMenu } from '../../reduxStore/appSlice';
-import { useEffect } from 'react';
+import React from "react";
+import VideoList from "./VideoList";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../../reduxStore/appSlice";
+import { useEffect } from "react";
 const VideoContainer = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className=' ml-16 flex  flex-wrap gap-3 h-full '>
+    <div className=" ml-16 flex  flex-wrap gap-3 h-full ">
       <VideoList />
     </div>
   );
