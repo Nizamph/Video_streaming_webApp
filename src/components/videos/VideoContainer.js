944c0eb844d51f161fd6049fc99ef77f647c0e31
Nizamph <nizamph7@gmail.com>
2025-02-27 10:46:17 +0530

    changes

--- src/components/videos/VideoContainer.js
+++ src/components/videos/VideoContainer.js
@@ -3,13 +3,16 @@ import VideoList from "./VideoList";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../../reduxStore/appSlice";
 import { useEffect } from "react";
+
 const VideoContainer = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
+
   return (
-    <div className=" ml-16 flex  flex-wrap gap-3 h-full ">
+    <div className="ml-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       <VideoList />
     </div>
   );
